20761aa31c033f5205f3263b9c9f52d85cdc9025
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-31 13:38:57 +0700

    juice: bringup: syberia

--- syberia_juice.mk
+++ syberia_juice.mk
@@ -5,22 +5,16 @@
 #
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
-
-# Inherit some Nitrogen-OS stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/nitrogen/products/common.mk)
 
-# Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+# Inherit some common Syberia stuff.
+$(call inherit-product, vendor/syberia/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nitrogen_juice
+PRODUCT_NAME := syberia_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := sm6115
+PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
